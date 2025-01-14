9199324ad33be0f85d93b438be99d2458676012a
@NehaRajput0502 <neharajput0502@gmail.com>
2025-01-14 16:20:58 +0530

    first python code

---
+++ hello.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n < 0:
+        return "Invalid input"
+    elif n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
